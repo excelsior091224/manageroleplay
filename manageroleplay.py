c8cfbc33ac1dcef7b869c865c5f2e6c8c1643a54
excelsior091224 <44745901+excelsior091224@users.noreply.github.com>
2020-09-13 14:29:09 +0900

    Update manageroleplay.py

--- manageroleplay.py
+++ manageroleplay.py
@@ -32,11 +32,11 @@ class MyClient(discord.Client):
         await self.send2developer(msg)
 
     #メンバーのステータスが変わったら通知する処理
-    async def on_member_update(self, before, after):
-        if before.status != after.status:
-            time = str(datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S"))
-            msg = "@everyone" + time + " " + after.display_name + " さんが " + str(after.status) + " になりました"
-            await self.get_channel(text_channel).send(msg)
+    #async def on_member_update(self, before, after):
+        #if before.status != after.status:
+            #time = str(datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S"))
+            #msg = "@everyone" + time + " " + after.display_name + " さんが " + str(after.status) + " になりました"
+            #await self.get_channel(text_channel).send(msg)
 
     #ダイス
     # メッセージを受信するごとに実行される
